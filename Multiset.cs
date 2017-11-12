e9d675b1b967df8f87512b9896860770f72c8953
bigmikey1994 <derekjeter94@gmail.com>
2017-11-11 18:29:54 -0800

    commit 3
    added skeleton functions so zack can work too

--- Multiset.cs
+++ Multiset.cs
@@ -6,10 +6,22 @@ namespace Numerical3DMatching
 {
 	class Multiset
 	{
-		public Multiset(int[] X, int[] Y, int[] Z, int score)
+		public int[] X, Y, Z;
+		public int score;
+		public Multiset(int[] X, int[] Y, int[] Z, int score = 0)
 		{
-
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+			this.score = score;
 		}
+		public Multiset MutateChild(Multiset child)
+		{
+			Multiset mutatedChild = child;
 
+			//mutate the child's values here
+
+			return mutatedChild;
+		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -4,13 +4,14 @@ namespace Numerical3DMatching
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+		static void Main(string[] args)
+		{
+			//init
 			int checkSum = 0;
 			int b = 10;
-			int[] X = {3,4,4 };
-			int[] Y = {1,4,6 };
-			int[] Z = {1,2,5 };
+			int[] X = { 3, 4, 4 };
+			int[] Y = { 1, 4, 6 };
+			int[] Z = { 1, 2, 5 };
 			foreach (int x in X)
 			{
 				checkSum = checkSum + x;
@@ -23,13 +24,24 @@ namespace Numerical3DMatching
 			{
 				checkSum = checkSum + z;
 			}
+			//check if valid
 			if (checkSum == X.Length * b)
 			{
 				Console.WriteLine("Check sum checks out.");
 			}
+			//Randomly create 1000k parents
+			Multiset initialSet = new Multiset(X, Y, Z);
+			//Console.Write("{{0},{1},{2}}, {{3},{4},{5}}, {{6},{7},{8}} Score: {9}/n", initialSet.X[0], initialSet.X[1], initialSet.X[2], initialSet.Y[0], initialSet.Y[1],initialSet.Y[2], initialSet.Z[0],initialSet.Z[1],initialSet.Z[2],initialSet.score);
 
 
+			//Create Children
+				//if child needs to be mutated, call Mutate Function
 
+
+			//Mutator Function
+
+
+			//end
 			Console.WriteLine("Hello World!");
 			Console.ReadKey(true);
         }
