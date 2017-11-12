1276966f10dfa5ae04de07033b7dd9bc22816f15
bigmikey1994 <derekjeter94@gmail.com>
2017-11-11 17:34:10 -0800

    beginning
    first

---
+++ Multiset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Numerical3DMatching
+{
+    class Multiset
+    {
+		public Multiset(int[] X, int[] Y, int[] Z, int score){
+			
+		}
+		
+	}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Numerical3DMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] X = {3,4,4 };
+			int[] Y = { };
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+			Console.ReadKey(true);
+        }
+    }
+
+}
