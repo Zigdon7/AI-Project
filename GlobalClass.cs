67cbe583a375cd186952af7532d610d649b54652
bigmikey1994 <derekjeter94@gmail.com>
2017-11-13 17:44:01 -0800

    checks each kid for mutation possibility
    ^

--- GlobalClass.cs
+++ GlobalClass.cs
@@ -5,24 +5,24 @@ namespace Numerical3DMatching
     {
         public static int b()
         {
-            return 10;
+            return 100;
         }
         public static int populationMultiplier()
         {
             return 100;
         }
         public static int[] Xvalue(){
-            int[] X = { 3, 4, 4 };
+            int[] X = { 36, 69, 14, 23, 33, 99 };
             return X;
         }
         public static int[] Yvalue()
         {
-            int[] Y = { 1, 4, 6 };
+            int[] Y = { 17, 30, 23, 35, 33, 0};
             return Y;
         }
         public static int[] Zvalue()
         {
-            int[] Z = { 1, 2, 5 };
+            int[] Z = { 57, 1, 54, 34, 51, 1 };
             return Z;
         }
         public static Multiset Initial(){
